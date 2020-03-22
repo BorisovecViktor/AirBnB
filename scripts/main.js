e2b83ee8452d5867529a037ab2f6defb417b26f9
BorisovecViktor <viktorborisovets@gmail.com>
2020-03-22 02:06:20 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const $btn = document.querySelector('.burger');
+const $nav = document.querySelector('.nav');
+const $link = document.querySelector('.nav__list');
+
+$btn.addEventListener('click', function() {
+  $nav.classList.toggle('active');
+  $btn.classList.toggle('open');
+});
+
+$link.addEventListener('click', function() {
+  $nav.classList.remove('active');
+  $btn.classList.remove('open');
+});
